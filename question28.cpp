b85f363957efc2a2472da24a32f2ff0180eebd8c
Soham Thapaliya <thapaliyasoham123@gmail.com>
2026-02-02 21:29:54 +0545

    question 28 cpp

---
+++ question28.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+void moveZeroes(vector<int> &nums)
+{
+    int j = 0;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] != 0)
+        {
+            swap(nums[i], nums[j]);
+            j++;
+        }
+    }
+}
+
+int main()
+{
+    vector<int> nums = {0, 1, 0, 3, 12};
+
+    moveZeroes(nums);
+
+    for (int x : nums)
+    {
+        cout << x << " ";
+    }
+
+    return 0;
+}
