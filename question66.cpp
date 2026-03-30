d9112614e46a22784517cb8f45c381203dded759
Soham Thapaliya <thapaliyasoham123@gmail.com>
2026-03-30 22:16:13 +0545

    update

---
+++ question66.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+void bfs(int start, vector<vector<int>> &adj, int n)
+{
+    vector<bool> visited(n, false);
+    queue<int> q;
+
+    visited[start] = true;
+    q.push(start);
+
+    while (!q.empty())
+    {
+        int node = q.front();
+        q.pop();
+
+        cout << node << " ";
+
+        for (int neighbor : adj[node])
+        {
+            if (!visited[neighbor])
+            {
+                visited[neighbor] = true;
+                q.push(neighbor);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n = 5;
+    vector<vector<int>> adj(n);
+
+    // Example graph
+    adj[0] = {1, 2};
+    adj[1] = {0, 3, 4};
+    adj[2] = {0};
+    adj[3] = {1};
+    adj[4] = {1};
+
+    bfs(0, adj, n);
+
+    return 0;
+}
