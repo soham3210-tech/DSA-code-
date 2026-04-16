c257cc4334fdfdece03d44b71e2196aa7b29dcec
Soham Thapaliya <thapaliyasoham123@gmail.com>
2026-04-16 19:45:21 +0545

    update

---
+++ e43.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int maxProfit(vector<int> &prices)
+{
+    int minPrice = INT_MAX;
+    int maxProfit = 0;
+
+    for (int price : prices)
+    {
+        minPrice = min(minPrice, price);
+        maxProfit = max(maxProfit, price - minPrice);
+    }
+    return maxProfit;
+}
+
+int main()
+{
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+    cout << maxProfit(prices);
+    return 0;
+}
