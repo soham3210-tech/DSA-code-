f7fb65142ad0bc6cf428f852e6c0a566b586aad6
Soham Thapaliya <thapaliyasoham123@gmail.com>
2026-08-19 23:03:53 +0545

    largest added

---
+++ largest.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {10, 25, 7, 45, 18};
+    int n = 5;
+
+    int largest = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+
+    cout << "Largest element = " << largest << endl;
+
+    return 0;
+}
